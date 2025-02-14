d3cccc63b93020210ae4730331bbc1f5ecc13628
K1enNg <hiimkien2702@gmail.com>
2025-02-14 13:18:30 -0500

    first commit

---
+++ gamevault_project/gamevault_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamevault_projectApp.swift
+//  gamevault_project
+//
+//  Created by Macbook de Kien on 2025-02-14.
+//
+
+import SwiftUI
+
+@main
+struct gamevault_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
